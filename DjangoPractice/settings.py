f6a873c071befa51e621356ebbc0b574563618a0
Adwait Tiwari <tiwariadwait3@gmail.com>
2025-08-26 23:57:42 +0530

    Day3 Learning

--- DjangoPractice/settings.py
+++ DjangoPractice/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Practice.apps.PracticeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
