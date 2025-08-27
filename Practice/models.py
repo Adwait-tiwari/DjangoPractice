7f93bde585b2ad9ad79818c1499c37b2d4248b9c
Adwait Tiwari <tiwariadwait3@gmail.com>
2025-08-27 23:56:53 +0530

    Day4 Learning

--- Practice/models.py
+++ Practice/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=122)
-    email = models.CharField(max_length=122)
-    subject = models.CharField(max_length=122)
-    desc = models.TextField()
+    name = models.CharField(max_length=122,blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    subject = models.CharField(max_length=122,blank=True, null=True)
+    desc = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name if self.name else "Anonymous"
--- Practice/views.py
+++ Practice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Practice.models import Contact
 
 # Create your views here.
 def home(request):
@@ -14,6 +15,13 @@ def about(request):
     # return HttpResponse("This is the about page")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,subject=subject,desc=desc)
+        contact.save()
     return render(request,'Contact.html')
 
     # return HttpResponse("this is the  contact page")
