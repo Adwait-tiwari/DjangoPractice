f4fc5d3c3f923996409f439d73b3452e60b42dc6
Adwait Tiwari <tiwariadwait3@gmail.com>
2025-08-28 23:03:19 +0530

    Day5 Learning

--- Practice/urls.py
+++ Practice/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('',views.home,name="home"),
     path('about',views.about,name="about"),
-    path('contact',views.contact,name="contact")
+    path('contact',views.contact,name="contact"),
+    path('login',views.LoginUser,name="login"),
+    path('logout',views.LogoutUser,name="logout")
 ]
--- Practice/views.py
+++ Practice/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from Practice.models import Contact
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
-    context = {
-        "variable" : "This is the Home page of everything"
-    }
-    return render(request,'Home.html',context)
+    if request.user.is_anonymous:
+        return redirect('/login')
+    return render(request,'Home.html')
     # return HttpResponse("This is the first Django Project")
 
 def about(request):
@@ -25,3 +26,22 @@ def contact(request):
     return render(request,'Contact.html')
 
     # return HttpResponse("this is the  contact page")
+
+def LoginUser(request):
+    if request.method == 'POST':
+         username = request.POST.get('username')
+         password =request.POST.get('password')
+
+         user = authenticate(request,username=username,password=password)
+
+         if user is not None:
+             login(request,user)
+             return redirect('/')
+         else:
+             return redirect('login')
+         
+    return render(request,'Login.html')
+
+def LogoutUser(request):
+    logout(request)
+    return render(request,'Logout.html')
